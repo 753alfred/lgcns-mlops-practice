ff8aa0806c6eba0e2be328877fd0a78d7dbeef95
753alfred <34301628+753alfred@users.noreply.github.com>
2023-08-04 01:07:45 +0000

    train

--- train.py
+++ train.py
@@ -23,13 +23,12 @@ from src.common.metrics import rmse_cv_score
 from src.common.utils import get_param_set
 from src.preprocess import preprocess_pipeline
 
-
-# 로그 들어갈 위치 
+# 로그 들어갈 위치
 # TODO: 로그를 정해진 로그 경로에 logs.log로 저장하도록 설정
+
 
 sys.excepthook = handle_exception
 warnings.filterwarnings(action="ignore")
-
 
 
 if __name__ == "__main__":
@@ -37,7 +36,7 @@ if __name__ == "__main__":
 
     _X = train_df.drop(["rent", "area_locality", "posted_on"], axis=1)
     y = np.log1p(train_df["rent"])
-    
+
     # TODO: X=_X, y=y로 전처리 파이프라인을 적용해 X에 저장
 
     # Data storage - 피처 데이터 저장
@@ -45,10 +44,8 @@ if __name__ == "__main__":
         os.makedirs(os.path.join(DATA_PATH, "storage"))
     X.assign(rent=y).to_csv(
         # TODO: DATA_PATH 밑에 storage 폴더 밑에 피처 데이터를 저장
-        
         index=False,
     )
-
 
     params_candidates = {
         "learning_rate": [0.01, 0.05, 0.1],
@@ -64,13 +61,13 @@ if __name__ == "__main__":
     mlflow.set_tracking_uri("./mlruns")
 
     for i, params in enumerate(param_set):
-
         run_name = f"Run {i}"
         with mlflow.start_run(run_name=f"Run {i}"):
             regr = GradientBoostingRegressor(**params)
             # 전처리 이후 모델 순서로 파이프라인 작성
             pipeline = Pipeline(
                 # TODO: 전처리 파이프라인와 모델을 파이프라인으로 묶을 것
+                [("preprocessor", preprocess_pipeline), ("regr", regr)]
             )
             pipeline.fit(_X, y)
 
@@ -86,7 +83,8 @@ if __name__ == "__main__":
             # 로깅 정보: 평가 메트릭
             mlflow.log_metrics(
                 {
-                    "RMSE_CV": #TODO: RMSE_CV 라는 이름으로 score_cv.mean()을 저장
+                    # "RMSE_CV": #TODO: RMSE_CV 라는 이름으로 score_cv.mean()을 저장
+                    "RMSE_CV": score_cv.mean(),
                 }
             )
 
@@ -123,10 +121,9 @@ if __name__ == "__main__":
 
     # TODO: 베스트 모델을 아티팩트 폴더에 복사
     copy_tree(
-              # TODO: 베스트 모델 URI에서 file:// 를 지울 것, 
-              ARTIFACT_PATH
+        # TODO: 베스트 모델 URI에서 file:// 를 지울 것,
+        ARTIFACT_PATH
     )
-
 
     # BentoML에 모델 저장
     bentoml.sklearn.save_model(
